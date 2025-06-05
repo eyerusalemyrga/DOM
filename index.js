c8dc32b4e84e67e12be54e1e5c70b3798440319f
Eyerusalem Yrga <eyerusalemykahsay>
2025-06-05 08:59:40 +0300

    added files

---
+++ index.js
@@ -0,0 +1,15 @@
+const containerFruit=document.getElementById('see-fruit');
+const containerVegetable=document.getElementById('see-vegetable');
+containerFruit.addEventListener('click', ()=>{
+    containerFruit.textContent='orange';
+    
+    
+});
+containerVegetable.addEventListener('click', ()=>{
+    containerVegetable.textContent='salad';
+    
+    
+});
+document.body.style.color='silver'
+const container=document.getElementById('title');
+container.style.color='green'
